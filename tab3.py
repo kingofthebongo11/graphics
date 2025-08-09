788a39aecf4c1083960b8aad463564d0937ec1ea
kingofthebongo11 <yanamid@yandex.ru>
2025-08-10 00:27:57 +0300

    Ensure Tkinter imports at top of tab3

--- tab3.py
+++ tab3.py
@@ -2,7 +2,6 @@ import tkinter as tk
 from tkinter import ttk
 from mylibproject.myutils_widgets import select_path
 
-
 def create_tab3(notebook, create_text, function1, clear_text):
     """Создает вкладку для извлечения частотных параметров из LS-DYNA."""
     # Создание третьей вкладки

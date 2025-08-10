79082420a60daea47afb91706dfa095c695e6857
kingofthebongo11 <yanamid@yandex.ru>
2025-08-10 15:37:35 +0300

    Revert "Добавлено чтение данных из Excel для построения графиков"

--- tabs/functions_for_tab1/curves_from_file/__init__.py
+++ tabs/functions_for_tab1/curves_from_file/__init__.py
@@ -1,11 +1,9 @@
 from .frequency_analysis import read_X_Y_from_frequency_analysis
 from .text_file import read_X_Y_from_text_file
 from .ls_dyna_file import read_X_Y_from_ls_dyna
-from .excel_file import read_X_Y_from_excel_file
 
 __all__ = [
     "read_X_Y_from_frequency_analysis",
     "read_X_Y_from_text_file",
     "read_X_Y_from_ls_dyna",
-    "read_X_Y_from_excel_file",
 ]
--- tabs/functions_for_tab1/events.py
+++ tabs/functions_for_tab1/events.py
@@ -13,8 +13,7 @@ def on_combobox_event(event, *callbacks):
 
 def on_combo_change_curve_type(frame, combo, label_curve_typeX, combo_curve_typeX, label_curve_typeY, combo_curve_typeY,
                                label_curve_typeX_type, combo_curve_typeX_type, label_curve_typeY_type,
-                               combo_curve_typeY_type, checkbox_horizontal=None, label_x_range=None,
-                               entry_x_range=None, label_y_range=None, entry_y_range=None):
+                               combo_curve_typeY_type):
     if combo.get() == "Частотный анализ":
         if not label_curve_typeX.winfo_viewable():  # Проверяем, отображается ли виджет
             frame.update_idletasks()
@@ -36,46 +35,6 @@ def on_combo_change_curve_type(frame, combo, label_curve_typeX, combo_curve_type
                                          y=combo.winfo_y() + 25)  # Отступ от параметра X
             combo_curve_typeY_type.place(x=combo_curve_typeX_type.winfo_x() + 170,
                                          y=combo.winfo_y() + 45, width=150)  # Позиция для Y
-        if checkbox_horizontal:
-            checkbox_horizontal.place_forget()
-        if label_x_range:
-            label_x_range.place_forget()
-            entry_x_range.place_forget()
-        if label_y_range:
-            label_y_range.place_forget()
-            entry_y_range.place_forget()
-    elif combo.get() == "Файл Excel":
-        label_curve_typeX.place_forget()
-        combo_curve_typeX.place_forget()
-        label_curve_typeY.place_forget()
-        combo_curve_typeY.place_forget()
-        label_curve_typeX_type.place_forget()
-        combo_curve_typeX_type.place_forget()
-        label_curve_typeY_type.place_forget()
-        combo_curve_typeY_type.place_forget()
-        if checkbox_horizontal:
-            frame.update_idletasks()
-            checkbox_horizontal.place(x=combo.winfo_x() + 170, y=combo.winfo_y())
-
-            def toggle_ranges():
-                if checkbox_horizontal.var.get():
-                    frame.update_idletasks()
-                    label_x_range.place(x=checkbox_horizontal.winfo_x() + 150,
-                                        y=combo.winfo_y() - 20)
-                    entry_x_range.place(x=checkbox_horizontal.winfo_x() + 150,
-                                        y=combo.winfo_y(), width=120)
-                    label_y_range.place(x=entry_x_range.winfo_x() + 130,
-                                        y=combo.winfo_y() - 20)
-                    entry_y_range.place(x=entry_x_range.winfo_x() + 130,
-                                        y=combo.winfo_y(), width=120)
-                else:
-                    label_x_range.place_forget()
-                    entry_x_range.place_forget()
-                    label_y_range.place_forget()
-                    entry_y_range.place_forget()
-
-            checkbox_horizontal.configure(command=toggle_ranges)
-            toggle_ranges()
     else:
         label_curve_typeX.place_forget()
         combo_curve_typeX.place_forget()
@@ -85,11 +44,3 @@ def on_combo_change_curve_type(frame, combo, label_curve_typeX, combo_curve_type
         combo_curve_typeX_type.place_forget()
         label_curve_typeY_type.place_forget()
         combo_curve_typeY_type.place_forget()
-        if checkbox_horizontal:
-            checkbox_horizontal.place_forget()
-        if label_x_range:
-            label_x_range.place_forget()
-            entry_x_range.place_forget()
-        if label_y_range:
-            label_y_range.place_forget()
-            entry_y_range.place_forget()

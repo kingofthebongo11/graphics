c0b29cdba635a77257395ddac11bec30b3d0a476
kingofthebongo11 <yanamid@yandex.ru>
2025-08-10 15:18:43 +0300

    Revert "Добавлена поддержка Excel-файлов для кривых"

--- tabs/functions_for_tab1/curves.py
+++ tabs/functions_for_tab1/curves.py
@@ -1,4 +1,3 @@
-import tkinter as tk
 from tkinter import ttk
 from widgets.select_path import select_path
 
@@ -10,7 +9,7 @@ def create_curve_box(input_frame, i, checkbox_var, saved_data):
     from widgets.text_widget import create_text  # локальный импорт для избегания циклической зависимости
 
     # Определяем высоту ячейки на основе состояния чекбокса
-    dy = 210 if checkbox_var.get() else 150
+    dy = 190 if checkbox_var.get() else 130
 
     # Метка о параметрах кривой
     label_curve_box = ttk.Label(input_frame, text=f"Настройка параметров кривой {i}:")
@@ -23,7 +22,7 @@ def create_curve_box(input_frame, i, checkbox_var, saved_data):
     # Создание выпадающего меню для типа кривой
     combo_curve_type = ttk.Combobox(
         input_frame,
-        values=["Частотный анализ", "Текстовой файл", "Файл кривой LS-Dyna", "Excel файл"],
+        values=["Частотный анализ", "Текстовой файл", "Файл кривой LS-Dyna"],
         state='readonly'
     )
     combo_curve_type.place(x=250, y=30 + dy * (i - 1), width=150)
@@ -69,6 +68,22 @@ def create_curve_box(input_frame, i, checkbox_var, saved_data):
     combo_curve_typeY_type.place(x=combo_curve_typeX_type.winfo_x() + 170,
                                  y=combo_curve_type.winfo_y() + 45, width=150)  # Позиция для оси Y
 
+    # Привязка события изменения выбора в combo_curve_type
+    combo_curve_type.bind("<<ComboboxSelected>>",
+                          lambda event: on_combobox_event(event,
+                                                          lambda e: on_combo_change_curve_type(input_frame,
+                                                                                               combo_curve_type,
+                                                                                               label_curve_typeX,
+                                                                                               combo_curve_typeX,
+                                                                                               label_curve_typeY,
+                                                                                               combo_curve_typeY,
+                                                                                               label_curve_typeX_type,
+                                                                                               combo_curve_typeX_type,
+                                                                                               label_curve_typeY_type,
+                                                                                               combo_curve_typeY_type),
+                                                          lambda e: saved_data[i - 1].update(
+                                                              {'curve_type': combo_curve_type.get()})))
+
     # Метка для выбора файла с кривой
     label_path = ttk.Label(input_frame, text="Выберите файл с кривой:")
     label_path.place(x=10, y=90 + dy * (i - 1))
@@ -78,61 +93,6 @@ def create_curve_box(input_frame, i, checkbox_var, saved_data):
     path_entry.place(x=10, y=110 + dy * (i - 1), width=600)
 
     path_entry._name = f"curve_{i}_filename"
-
-    # Элементы для Excel файлов
-    horizontal_var = tk.IntVar()
-    horizontal_check = ttk.Checkbutton(input_frame, text="По горизонтали", variable=horizontal_var)
-    horizontal_check.var = horizontal_var
-    horizontal_check._name = f"curve_{i}_horizontal"
-
-    label_X_range = ttk.Label(input_frame, text="Диапазон X:")
-    entry_X_range = create_text(input_frame, method="entry", height=1, state='normal', scrollbar=False)
-    entry_X_range._name = f"curve_{i}_X_range"
-    label_Y_range = ttk.Label(input_frame, text="Диапазон Y:")
-    entry_Y_range = create_text(input_frame, method="entry", height=1, state='normal', scrollbar=False)
-    entry_Y_range._name = f"curve_{i}_Y_range"
-
-    def toggle_ranges():
-        if horizontal_var.get():
-            y_pos = path_entry.winfo_y() + 30
-            label_X_range.place(x=10, y=y_pos)
-            entry_X_range.place(x=110, y=y_pos, width=100)
-            label_Y_range.place(x=230, y=y_pos)
-            entry_Y_range.place(x=330, y=y_pos, width=100)
-        else:
-            label_X_range.place_forget()
-            entry_X_range.place_forget()
-            label_Y_range.place_forget()
-            entry_Y_range.place_forget()
-
-    horizontal_check.config(command=toggle_ranges)
-
-    # Привязка события изменения выбора в combo_curve_type
-    combo_curve_type.bind(
-        "<<ComboboxSelected>>",
-        lambda event: on_combobox_event(
-            event,
-            lambda e: on_combo_change_curve_type(
-                input_frame,
-                combo_curve_type,
-                label_curve_typeX,
-                combo_curve_typeX,
-                label_curve_typeY,
-                combo_curve_typeY,
-                label_curve_typeX_type,
-                combo_curve_typeX_type,
-                label_curve_typeY_type,
-                combo_curve_typeY_type,
-                path_entry,
-                horizontal_check,
-                label_X_range,
-                entry_X_range,
-                label_Y_range,
-                entry_Y_range,
-            ),
-            lambda e: saved_data[i - 1].update({'curve_type': combo_curve_type.get()})
-        )
-    )
 
     # Кнопка для выбора файла
     select_button = ttk.Button(input_frame, text="Выбор файла",
@@ -162,13 +122,13 @@ def update_curves(frame, num_curves, next_frame, checkbox_var, saved_data):
         num_curves_int = int(num_curves)
 
     # Меняем высоту фрейма в зависимости от количества кривых
-    frame_height = 230 * num_curves_int if checkbox_var.get() else 170 * num_curves_int
+    frame_height = 210 * num_curves_int if checkbox_var.get() else 150 * num_curves_int
     frame.place_configure(height=frame_height)
 
     # Восстанавливаем данные, если они есть
     for i in range(len(saved_data), num_curves_int):
         saved_data.append({'curve_type': "", 'path': "", 'legend': "", 'curve_typeX': "", 'curve_typeY': "",
-                           'curve_typeX_type': "", 'curve_typeY_type': "", 'horizontal': 0, 'X_range': "", 'Y_range': ""})
+                           'curve_typeX_type': "", 'curve_typeY_type': ""})
 
     for i in range(1, num_curves_int + 1):
         create_curve_box(frame, i, checkbox_var, saved_data)
--- tabs/functions_for_tab1/plotting.py
+++ tabs/functions_for_tab1/plotting.py
@@ -6,7 +6,6 @@ from .curves_from_file import (
     read_X_Y_from_frequency_analysis,
     read_X_Y_from_text_file,
     read_X_Y_from_ls_dyna,
-    read_X_Y_from_excel,
 )
 
 
@@ -98,8 +97,6 @@ def get_X_Y_data(curve_info):
         read_X_Y_from_text_file(curve_info)
     elif curve_info['curve_type'] == 'Файл кривой LS-Dyna':
         read_X_Y_from_ls_dyna(curve_info)
-    elif curve_info['curve_type'] == 'Excel файл':
-        read_X_Y_from_excel(curve_info)
 
 
 def generate_graph(ax, fig, canvas, path_entry_title, combo_titleX, combo_titleX_size, combo_titleY, combo_titleY_size,
@@ -146,13 +143,6 @@ def generate_graph(ax, fig, canvas, path_entry_title, combo_titleX, combo_titleX
                 # Получаем имя файла для каждой кривой
                 if widget_name == f"curve_{i}_filename":
                     curve_info['curve_file'] = widget.get()
-
-                if widget_name == f"curve_{i}_horizontal":
-                    curve_info['horizontal'] = widget.var.get()
-                elif widget_name == f"curve_{i}_X_range":
-                    curve_info['X_range'] = widget.get()
-                elif widget_name == f"curve_{i}_Y_range":
-                    curve_info['Y_range'] = widget.get()
 
                 # Проверяем наличие легенды, если отмечен чекбокс
                 if legend_checkbox.get() and widget_name == f"curve_{i}_legend":

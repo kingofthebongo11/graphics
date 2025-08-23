6b6be11854b851c3db76a6695fa5ecba3a591cc3
kingofthebongo11 <yanamid@yandex.ru>
2025-08-23 19:18:09 +0300

    test: switch to format_signature

--- tests/test_create_plot_fontstyle.py
+++ tests/test_create_plot_fontstyle.py
@@ -1,15 +1,19 @@
 from pathlib import Path
+import shutil
 import sys
 
 import matplotlib
 matplotlib.use('Agg')
 import matplotlib.pyplot as plt
+from unittest.mock import patch
+
+import pytest
 
 sys.path.append(str(Path(__file__).resolve().parent.parent))
 from tabs.function_for_all_tabs import create_plot
-from unittest.mock import patch
 
 
+@pytest.mark.skipif(shutil.which("latex") is None, reason="LaTeX not installed")
 def test_default_title_fontstyle_normal():
     fig, ax = plt.subplots()
     curves = [{"X_values": [0, 1], "Y_values": [0, 1]}]

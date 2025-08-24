af16a1f9538c89bdef08c4dccede14950804240e
kingofthebongo11 <yanamid@yandex.ru>
2025-08-24 23:44:37 +0300

    Обновлены типы анализа

--- analysis_types.py
+++ analysis_types.py
@@ -2,12 +2,20 @@ from enum import Enum
 
 class AnalysisType(str, Enum):
     """Допустимые типы анализа."""
-    TIME_AXIAL_FORCE = "Время-Продольное усилие"
-    TIME_SHEAR_FORCE_QY = "Время-Поперечная сила QY"
-    TIME_SHEAR_FORCE_QZ = "Время-Поперечная сила QZ"
-    TIME_MOMENT_MX = "Время-Момент MX"
-    TIME_MOMENT_MY = "Время-Момент MY"
-    TIME_MOMENT_MZ = "Время-Момент MZ"
+    TIME_AXIAL_FORCE = "Время-Продольная сила"
+    TIME_SHEAR_FORCE_Y = "Время-Поперечная сила по Y"
+    TIME_SHEAR_FORCE_Z = "Время-Поперечная сила по Z"
+    TIME_BENDING_MOMENT_MS_MY = "Время-Изгибающий момент Ms (My)"
+    TIME_BENDING_MOMENT_MT_MZ = "Время-Изгибающий момент Mt (Mz)"
+    TIME_TORQUE_MX = "Время-Крутящий момент Mx"
+    TIME_NORMAL_STRESS_X = "Время-Нормальное напряжение X"
+    TIME_SHEAR_STRESS_XY = "Время-Касательное напряжение XY"
+    TIME_SHEAR_STRESS_ZX = "Время-Касательное напряжение ZX"
+    TIME_PLASTIC_STRAIN_INTENSITY = (
+        "Время-Интенсивность пластических деформаций"
+    )
+    TIME_ELONGATION = "Время-Удлинение"
+    TIME_STRESS_INTENSITY = "Время-Интенсивность напряжений"
 
     @classmethod
     def list(cls) -> list[str]:
